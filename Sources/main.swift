dfc5e80e1e3e60fe10f7d09496cf1f57c874cada
Charles Hart <charleshart.lds@gmail.com>
2016-03-22 22:57:14 -0600

    start ascii stuff

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,23 @@
 
 import Foundation
 
+extension String {
+    var asciiValue: UInt32 {
+        guard let first = characters.first where characters.count == 1 else  { return 0 }
+        return first.unicodeScalarsValue
+    }
+    func asciiValueAt(pos pos: UInt32) -> UInt32 {
+        guard characters.count > 0 && Int(pos) < characters.count else  { return 0 }
+        return Array(characters)[Int(pos)].unicodeScalarsValue
+    }
+}
+extension Character {
+    var unicodeScalarsValue: UInt32 {
+        return String(self).unicodeScalars.first!.value
+    }
+}
+
+
 func publicKey() -> Int {
 	return 3
 }
@@ -14,6 +31,7 @@ if Process.arguments.count == 2 {
 	let messageToEncrypt = Process.arguments[1]
 	print("Public Key : \(publicKey())")
 	print("Private Key : \(privateKey())")
+	print("ascii message : \(asciiMessage(messageToEncrypt))")
 	print("Encrypted Text : SFDNA-\(messageToEncrypt)-FSJDAN")
 }
 
